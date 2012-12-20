6f0a381d5e195ca59faf20c75a4974a014d28932
Paul Garner <ego@anentropic.com>
2012-12-20 18:33:51 +1300

    pypi v0.1.2 adds management commands

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-ebaysync',
-    version='0.1.0',
+    version='0.1.2',
     packages=['ebaysync',],
     license='LGPL v3',
     long_description=open('pypi.rst').read(),
