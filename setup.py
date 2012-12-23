3d6ce64d428bb12ef11c3765bdfe42691c910037
Paul Garner <ego@anentropic.com>
2012-12-23 22:36:53 +1300

    pypi version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-ebaysync',
-    version='0.1.12',
+    version='0.1.13',
     packages=[
         'ebaysync',
         'ebaysync.management',
