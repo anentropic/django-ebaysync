bc9f8d512ba358e1e8bc00cc53e113e46fb7f287
Paul Garner <ego@anentropic.com>
2012-12-29 23:58:25 +1300

    fix notification types

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-ebaysync',
-    version='0.2.0',
+    version='0.2.1',
     packages=[
         'ebaysync',
         'ebaysync.management',
