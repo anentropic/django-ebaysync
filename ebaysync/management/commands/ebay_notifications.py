242e35cc1239b4a875a379a40dc81e0af0f8e302
anentropic <ego@anentropic.com>
2013-06-01 20:56:12 +0100

    fix up enable disable stuff in ebay_notifications

--- ebaysync/management/commands/ebay_notifications.py
+++ ebaysync/management/commands/ebay_notifications.py
@@ -7,6 +7,15 @@ from ...views import get_notification_url
 from ...models import UserToken
 
 
+# if these keys are in options we want to set prefs, not get them
+SET_PREFS_KEYS = (
+    'disable_markdown_alerts',
+    'disable_application',
+    'enable_markdown_alerts',
+    'enable_application',
+)
+
+
 class Command(BaseCommand):
     args = '<notification_type notification_type ...>'
     help = 'Call without args to GetNotificationPreferences, with args to SetNotificationPreferences.'
@@ -22,10 +31,15 @@ class Command(BaseCommand):
         make_option('--disable', action='store_true',
                     help='Disable the specified notification types rather than enabling them'),
         make_option('--disable-application', action='store_true',
-                    help='Disable all notifications for the app (while preserving notification preferences)'),
+                    help='Disable all notifications for the app (while preserving individual notification preferences)'),
         make_option('--disable-markdown-alerts', action='store_true',
                     help="Disable the sending of alerts when eBay disables notifications (e.g. due to\
                           undeliverability) for your app. You probably don't want this."),
+        make_option('--enable-application', action='store_true',
+                    help='Set to enable notifications for the app (while preserving individual notification preferences)'),
+        make_option('--enable-markdown-alerts', action='store_true',
+                    help="Set enabled the sending of alerts when eBay disables notifications (e.g. due to\
+                          undeliverability) for your app."),
     )
 
     def handle(self, *args, **options):
@@ -41,14 +55,24 @@ class Command(BaseCommand):
             ebay_kwargs['sandbox'] = user.is_sandbox
         client = TradingAPI(**ebay_kwargs)
 
-        # if these keys are in options we want to set prefs, not get them
-        set_prefs_keys = ('disable-markdown-alerts', 'disable-application')
+        alert_enable = True
+        if options['disable_markdown_alerts']:
+            alert_enable = False
+        if options['enable_markdown_alerts']:
+            alert_enable = True
 
-        if args or any((True for key in options if key in set_prefs_keys)):
+        notifications_enable = True
+        if options['disable_application']:
+            notifications_enable = False
+        if options['enable_application']:
+            notifications_enable = True
+
+        if args or any((True for (k,v) in options.items() if k in SET_PREFS_KEYS and v is not None)):
+            print "SET"
             app_prefs = client.sudsclient.factory.create('ApplicationDeliveryPreferencesType')
-            app_prefs.AlertEnable = 'Disable' if options['disable-markdown-alerts'] else 'Enable'
+            app_prefs.AlertEnable = 'Enable' if alert_enable else 'Disable'
             app_prefs.ApplicationURL = get_notification_url(username=options['for'])
-            app_prefs.ApplicationEnable = 'Disable' if options['disable-application'] else 'Enable'
+            app_prefs.ApplicationEnable = 'Enable' if notifications_enable else 'Disable'
             # these fields are optional in the API but suds sends empty keys
             # for them if we don't give values here
             app_prefs.DeviceType = 'Platform'
@@ -67,6 +91,7 @@ class Command(BaseCommand):
             )
             print response.Ack
         else:
+            print "GET"
             app_prefs = client.GetNotificationPreferences(PreferenceLevel='Application')
             print 'Application:\n%s' % app_prefs.__str__()
             user_prefs = client.GetNotificationPreferences(PreferenceLevel='User')
